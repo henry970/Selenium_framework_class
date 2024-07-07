5b1c49f15492422a3cf2f9b5c9067808ae9c2b4d
henry970 <henryokolie615@gmail.com>
2024-07-07 10:20:50 +0100

    update test

---
+++ LoginLocators/LoginLocator_test.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+
+
+class LoginLocators:
+    ENTER_EMAIL_ADDRESS = (By.ID, "")
+    ENTER_PASSWORD = (By.ID, "")
+    REMEMBER_CHECK_BOX = (By.ID, "")
+    CLICK_SUBMIT_BUTTON = (By.ID, "")
